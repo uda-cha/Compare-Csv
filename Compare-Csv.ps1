c7754c1e66df27ddb780bdad618e5acd37c53001
uda-cha <mine_s@zd6.so-net.ne.jp>
2018-05-25 01:05:16 +0900

    Add support for PowerShell 2.0

--- Compare-Csv.ps1
+++ Compare-Csv.ps1
@@ -1,8 +1,10 @@
 #二つ目の引数に取ったcsvのcsvヘッダを配列として格納
+#[todo]ヘッダ比較して異なるならエラー返す
 $headerstr = Get-Content $args[1] | Select-Object -First 1
 $headerarray = $headerstr.split(",")
 
 #比較対象から除外するヘッダのリスト
+#[todo]スイッチにしたい
 $excludeheader = @( `
   "CVE", `
   "CVSS", `
@@ -13,38 +15,17 @@ $excludeheader = @( `
   "See Also" `
   )
 
-#長文比較対象リスト
+#長文比較対象リスト。セル単位ではなくセルの中身を一行ずつ比較する
+#[todo]スイッチにしたい
 $longheader = @( `
-  "Plugin Output" `  #長い文章のためうまく比較できない。文字列として別に比較する
+  "Plugin Output"
   )
 
 #csv読み込み
-$A = Import-Csv -Encoding UTF8 -Path $args[0]
-$B = Import-Csv -Encoding UTF8 -Path $args[1]
+$A = Import-Csv -Path $args[0]
+$B = Import-Csv -Path $args[1]
 
 
-##整備中
-#Write-Output "以下のcsvヘッダを除外して比較します。"
-#Write-Output $excludeheader
-#Write-Output ""
-#Write-Output "-------------------------"
-#Write-Output ""
-#
-#for ( $rownumB=0; $rownumB -le $B.Count; $rownumB++ )
-#  {
-#  for ( $headernum=0; $headernum -le $headerarray.Count; $headernum++ )
-#    {
-#    if ( ! ($A.($headerarray[$headernum]) -contains $B[$rownumB].($headerarray[$headernum])) )
-#      {
-#        Write-Output $headerarray[$headernum]
-#        Write-Host $B[$rownumB].($headerarray[$headernum]) -ForegroundColor Cyan
-#      }
-#    }
-#  }
-#
-#exit
-
-#main
 Write-Output "以下のcsvヘッダを除外して比較します。"
 Write-Output $excludeheader
 Write-Output ""
@@ -54,18 +35,16 @@ Write-Output ""
 foreach ( $targetheader in $headerarray )
   {
   
-  #長文比較対象リストにあるヘッダの比較
+  #長文比較対象リストにあるヘッダ列の比較。セル単位ではなくセルの中身の一行ずつ比較
   if ( $longheader -contains $targetheader )
     {
     
-    $longA = [string]$A.$targetheader
-    $longB = [string]$B.$targetheader
-    #Compare-Object $longA $longB | Format-Table -Wrap -AutoSize 
-      $longA > longA.txt
-      $longB > longB.txt
+    #Windiows7対応。10のPSv3なら$A.$targetheaderでOK
+    [string]( $A | Select-Object $targetheader | Format-Table -Wrap -AutoSize | Out-String ) > longA.txt
+    [string]( $B | Select-Object $targetheader | Format-Table -Wrap -AutoSize | Out-String ) > longB.txt
       Compare-Object (Get-Content .\longA.txt) (Get-Content .\longB.txt) `
         | Format-Table -Wrap -AutoSize -Property SideIndicator,@{name="$targetheader";expression={$_.InputObject}}
-      Remove-Item Long*.txt
+      Remove-Item "Long[A|B].txt"
     Write-Output "-------------------------"
     Write-Output ""
     continue
@@ -77,7 +56,7 @@ foreach ( $targetheader in $headerarray )
     continue
     }
   
-  #通常の比較
+  #通常の比較。セル単位で比較
   Compare-Object $A $B -Property $targetheader  `
     | Format-Table -Wrap -AutoSize -Property SideIndicator,$targetheader
   Write-Output "-------------------------"
